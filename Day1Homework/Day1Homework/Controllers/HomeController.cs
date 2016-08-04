c32c67143729b0dedde73e3063fbe069335ee173
ywduck <yw.duck@gmail.com>
2016-08-04 15:18:18 +0800

    新增ChildAction

--- Day1Homework/Day1Homework/Controllers/HomeController.cs
+++ Day1Homework/Day1Homework/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Day1Homework.Models.ViewModels;
+
 namespace Day1Homework.Controllers
 {
     public class HomeController : Controller
@@ -25,6 +27,34 @@ namespace Day1Homework.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MoneyList() {
+            List<MoneyRecViewModel> listMoneyRec = new List<MoneyRecViewModel>();
+            listMoneyRec.Add(new MoneyRecViewModel()
+            {
+                Id = "1",
+                MoneyType = "支出",
+                CreDate = DateTime.Now.ToString("yyyy/MM/dd"),
+                Amount = 300
+            });
+            listMoneyRec.Add(new MoneyRecViewModel()
+            {
+                Id = "2",
+                MoneyType = "支出",
+                CreDate = DateTime.Now.AddDays(1).ToString("yyyy/MM/dd"),
+                Amount = 1600
+            });
+            listMoneyRec.Add(new MoneyRecViewModel()
+            {
+                Id = "3",
+                MoneyType = "支出",
+                CreDate = DateTime.Now.AddDays(2).ToString("yyyy/MM/dd"),
+                Amount = 600
+            });
+
+            return View(listMoneyRec);
         }
     }
 }
